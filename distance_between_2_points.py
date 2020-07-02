447d7219cfe0f6e36c88d2066e4eddfa6816757b
sg23600 <sujaygoswami@hotmail.com>
2020-07-02 12:52:45 +0530

    added hamming distance function

--- distance_between_2_points.py
+++ distance_between_2_points.py
@@ -14,3 +14,11 @@ def manhattan_distance(pt1, pt2):
     for i in range(len(pt1)):
         distance += abs(pt1[i] - pt2[i])
     return distance
+
+#hamming
+def hamming_distance(pt1, pt2):
+    distance = 0
+    for i in range(len(pt1)):
+        if pt1[i] != pt2[i]:
+            distance += 1
+    return distance
