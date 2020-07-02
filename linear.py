98ff0f36646bc0b33db9b19ad45354272c6eddcf
sg23600 <sujaygoswami@hotmail.com>
2020-07-02 21:49:53 +0530

    added main gradient descent function

--- linear.py
+++ linear.py
@@ -6,7 +6,7 @@ y_predicted = [m * x + b for x in x]
 total_loss = 0
 total_loss += [(y[i] - y_predicted[i]) ** 2 for i in range(len(y))]
 
-# gradient descent
+# gradient descent for parameters
 def get_gradient_at_b(x, y, m, b):
     diff = 0
     N = len(x)
@@ -25,9 +25,17 @@ def get_gradient_at_m(x, y, m, b):
     return m_gradient
 
 # step gradient
-def step_gradient(x, y, b_current, m_current):
+def step_gradient(b_current, m_current, x, y, learning_rate):
     b_gradient = get_gradient_at_b(x, y, b_current, m_current)
     m_gradient = get_gradient_at_m(x, y, b_current, m_current)
-    b = b_current - (0.01 * b_gradient)
-    m = m_current - (0.01 * m_gradient)
+    b = b_current - (learning_rate * b_gradient)
+    m = m_current - (learning_rate * m_gradient)
+    return [b, m]
+
+# gradient decent
+def gradient_descent(x, y, learning_rate, num_iterations):
+    b = 0
+    m = 0
+    for _ in range(num_iterations):
+        b, m = step_gradient(b, m, x, y, learning_rate)
     return [b, m]
