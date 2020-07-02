037342d1f0798aefdadf34bbbf0fae65c8dc038f
sg23600 <sujaygoswami@hotmail.com>
2020-07-02 12:00:58 +0530

    added manhattan distance function

--- distance_between_2_points.py
+++ distance_between_2_points.py
@@ -7,3 +7,10 @@ def euclidean_distance(pt1, pt2):
 
     distance = distance ** 0.5
     return distance
+
+# manhattan
+def manhattan_distance(pt1, pt2):
+    distance = 0
+    for i in range(len(pt1)):
+        distance += abs(pt1[i] - pt2[i])
+    return distance
