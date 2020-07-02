accacb700c96b621277ee0a81e357ca8ad6bfba3
sg23600 <sujaygoswami@hotmail.com>
2020-07-02 20:21:38 +0530

    added gradient descent function

--- linear.py
+++ linear.py
@@ -1,8 +1,17 @@
 # m = slope
 # b = y-intercept
-y_predicted = [m * x + b for x in x]\
+y_predicted = [m * x + b for x in x]
 
-#loss is sum of sqaured vertical distances b/w predicted and actual values
+# loss is sum of sqaured vertical distances b/w predicted and actual values
 total_loss = 0
 total_loss += [(y[i] - y_predicted[i]) ** 2 for i in range(len(y))]
+
+# gradient descent
+def get_gradient_at_b(x, y, m, b):
+    diff = 0
+    for i in range(len(x)):
+        diff += y[i] - (m * x[i] + b)
+
+    b_gradient = (-2 / len(x)) * diff
+    return b_gradient
 
